61000b55ba13811a5abe01752aacbec29bd4ba8b
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-10 00:10:44 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+end
+
+def bark
+    puts "Woof!"
+  end
+
+def sit
+   puts "The Dog is sitting"
+  end
+
+fido = Dog.new
+puts fido
+
+snoopy = Dog.new
+puts snoopy
+
+lassie = Dog.new
+puts lassie
