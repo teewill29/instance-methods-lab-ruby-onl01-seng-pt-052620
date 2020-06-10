70536bf443d2a35b7f3d3257739bcaaf35a525b9
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-10 00:11:12 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+ end
+
+ def talk
+   puts "Hello World!"
+ end
+
+def walk
+   puts "The Person is walking"
+ end
+
+adele_goldberg = Person.new
+puts adele_goldberg
+
+alan_kay = Person.new
+puts alan_kay
